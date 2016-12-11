1331be5e8cab3d7fd0966923418459e9b34ccc5f
Ammar Sherif <ammarsherif90@gmail.com>
2016-12-11 23:52:21 +0200

    Added Breadth first

--- src/gsaa/SerializableCityInfo.java
+++ src/gsaa/SerializableCityInfo.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package gsaa;
 
@@ -9,7 +7,7 @@ import java.io.Serializable;
 
 /**
  *
- * @author family
+ * @author Ammar Sherif
  */
 public class SerializableCityInfo implements Serializable{
     private String cityName;
